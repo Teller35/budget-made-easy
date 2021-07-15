3d28576348ae3b05c8164f61b073996ed3678501
Teller35 <tellerwetzel@yahoo.com>
2021-07-15 13:49:55 -0600

    working on caches and getting page to load offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,69 @@
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "V-1";
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE = APP_PREFIX + "data-V1";
 
-const FILES_TO_CACHE = ["./index.html", "./js/index.js", "./css/styles.css"];
+const FILES_TO_CACHE = [
+  "/",
+  "./js/index.js",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./icons/icon-192x192.png",
+];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Installing cache: " + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Installing Cache: " + CACHE_NAME);
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+      .catch((err) => console.log("Error caching files: ", err))
   );
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeep = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeep.push(CACHE_NAME);
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeep.indexOf(key) === -1) {
-            console.log("Deleting cache: " + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
+    caches
+      .keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE) {
+              console.log("Deleting cache: " + key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+      .catch((err) => console.log(err))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE)
+        .then((cache) => {
+          console.log(cache);
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.cone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              console.log(err);
+              return cache.match(event.target);
+            });
         })
-      );
-    })
-  );
+        .catch((err) => console.log(err))
+    );
+  }
+  
 });
-
