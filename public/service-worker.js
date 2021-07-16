b884b7ecf71872f924b82ac81bb021e31ff7c73c
Teller35 <tellerwetzel@yahoo.com>
2021-07-16 12:54:42 -0600

    trying to get service worker to cache landing page and js files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,11 +3,12 @@ const VERSION = "V-1";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "/",
+  "/api/transaction",
   "./index.html",
-  "./css/styles.css",
+  "./manifest.json",
   "./js/idb.js",
   "./js/index.js",
+  "./css/styles.css",
   "./icons/icon-192x192.png",
   "./icons/icon-128x128.png",
   "./icons/icon-144x144.png",
@@ -19,45 +20,45 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Installing cache: " + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log("Installing cache : " + CACHE_NAME);
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  });
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeepList = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeepList.push(CACHE_NAME);
+// self.addEventListener("activate", function (event) {
+//   event.waitUntil(
+//     caches.keys().then(function (keyList) {
+//       let cacheKeepList = keyList.filter(function (key) {
+//         return key.indexOf(APP_PREFIX);
+//       });
+//       cacheKeepList.push(CACHE_NAME);
 
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeepList.indexOf(key) === -1) {
-            console.log("Deleting cache: " + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
-    })
-  );
-});
+//       return Promise.all(
+//         keyList.map(function (key, i) {
+//           if (cacheKeepList.indexOf(key) === -1) {
+//             console.log("Deleting cache: " + keyList[i]);
+//             return caches.delete(keyList[i]);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
-self.addEventListener("fetch", function (event) {
-  console.log("Fetch request:" + event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function (request) {
-      if (request) {
-        console.log("Responding with cache: " + event.request.url);
-        return request;
-      } else {
-        console.log("File is not cached, fetching: " + event.request.url);
-        return fetch(event.request);
-      }
-    })
-  );
-});
+// self.addEventListener("fetch", function (event) {
+//   console.log("Fetch request:" + event.request.url);
+//   event.respondWith(
+//     caches.match(event.request).then(function (request) {
+//       if (request) {
+//         console.log("Responding with cache: " + event.request.url);
+//         return request;
+//       } else {
+//         console.log("File is not cached, fetching: " + event.request.url);
+//         return fetch(event.request);
+//       }
+//     })
+//   );
+// });
